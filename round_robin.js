40d3c0b96dcebbbaaa209bcbab0a8f2bfc6d585f
Arsalan Khattak <akkhattak65@gmail.com>
2020-12-24 15:20:58 +0500

    Add WT and TAT

--- round_robin.js
+++ round_robin.js
@@ -1,49 +1,94 @@
 const prompt = require("prompt");
 const Table = require("cli-table");
+const chalk = require("chalk");
 prompt.start();
 
 init();
 
 async function init() {
-  const burstTimes = [];
+  console.clear();
+  console.log(
+    chalk.blue.bold("Round Robin"),
+    "-",
+    chalk.red("Without Arrival Time")
+  );
+  const burstTimes = [24, 3, 3];
   const ganttChart = [];
+  const turnAroundTime = [];
+  const waitingTime = [];
+  let averageTAT = 0,
+    averageWT = 0;
   let isRemaining = true;
-  const { processors, quantum } = await prompt.get(["processors", "quantum"]);
+  let isCompleted = false;
+  const initialInput = await prompt.get(["processors", "quantum"]);
+  const processors = Number(initialInput.processors);
+  const quantum = Number(initialInput.quantum);
 
   for (let i = 0; i < processors; i++) {
     const getInput = await prompt.get([`Burst Time of P${i + 1}`]);
-    burstTimes[i] = getInput[Object.keys(getInput)[0]];
+    burstTimes[i] = Number(getInput[Object.keys(getInput)[0]]);
   }
+
+  const cloneBurstTimes = [...burstTimes];
 
   while (isRemaining) {
     isRemaining = false;
     for (let i = 0; i < burstTimes.length; i++) {
       if (burstTimes[i] < 1) continue;
       let currVal = quantum;
-      if (burstTimes[i] < quantum) {
+      if (burstTimes[i] <= quantum) {
         currVal = burstTimes[i];
+        isCompleted = true;
       }
       if (burstTimes[i] > quantum) {
         isRemaining = true;
+        isCompleted = false;
       }
       burstTimes[i] = burstTimes[i] - quantum;
       ganttChart.push({
         Processor: `P${i + 1}`,
         value: currVal,
+        isCompleted,
+        process: i,
       });
     }
   }
   console.log();
-  const table = new Table({
+  const ganttChartTable = new Table({
     head: ["Processor", "Timing"],
-    // colWidths: [100, 100],
+  });
+  const processTable = new Table({
+    head: ["Processor", "Completion Time", "Turn Around Time", "Waiting Time"],
   });
   let timing = 0;
   for (let i = 0; i < ganttChart.length; i++) {
     timing += Number(ganttChart[i].value);
-    table.push([ganttChart[i].Processor, timing]);
+    ganttChartTable.push([ganttChart[i].Processor, timing]);
+    // Calculate Turn Around Time and Waiting Time
+    if (ganttChart[i].isCompleted) {
+      turnAroundTime[ganttChart[i].process] = timing;
+
+      waitingTime[ganttChart[i].process] =
+        timing - cloneBurstTimes[ganttChart[i].process];
+
+      averageTAT += timing;
+      averageWT += waitingTime[ganttChart[i].process];
+    }
   }
-  console.log(table.toString());
+
+  for (let i = 0; i < cloneBurstTimes.length; i++) {
+    processTable.push([`P${i + 1}`, timing, turnAroundTime[i], waitingTime[i]]);
+  }
+  averageTAT /= cloneBurstTimes.length;
+  averageTAT = averageTAT.toFixed(2);
+
+  averageWT /= cloneBurstTimes.length;
+  averageWT = averageWT.toFixed(2);
+
+  console.log(ganttChartTable.toString());
+  console.log(processTable.toString());
+  console.log("Average TAT: ", averageTAT);
+  console.log("Average WT: ", averageWT);
 }
 
 /*
@@ -74,5 +119,8 @@ P1 -> 8
 P2 -> 9
 P3 -> 11
 P3 -> 11
+
+Turn Around Time = Completion Time - Arrival Time
+Waiting Time = Turn Around Time - Burst Time
 
 */
